c61ad60bfc1b7400361a8191eb8eca2012e2b403
Valeria Rodriguez <val.lilou.rod@gmail.com>
2021-04-21 11:05:17 -0600

    script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,65 +1,88 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// constants for password criterias
+const passwordText = document.querySelector("#password");
+const passwordLength = document.querySelector("#length");
+const passwordUpper = document.querySelector("#checkUpper");
+const passwordLower = document.querySelector("#checkLower");
+const passwordNumber = document.querySelector("#checkNumeric");
+const passwordSpecial = document.querySelector("#checkSpecial");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+const randomPass = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
 
-//WHEN I click the button to generate a password a popup window is displayed
-function criteriaLower() {
-  var checkBox = document.getElementById("checkLower");
-  if (checkBox.checked == true) {
-      text.style.display = "block";
- } else {
-       text.style.display = "none";
-   }
-};
-function criteriaUpper() {
-  var checkBox = document.getElementById("checkUpper");
-  if (checkBox.checked == true) {
-      text.style.display = "block";
- } else {
-       text.style.display = "none";
-   }
-};
-function criteriaNumeric() {
-  var checkBox = document.getElementById("checkNumeric");
-  if (checkBox.checked == true) {
-      text.style.display = "block";
- } else {
-       text.style.display = "none";
-   }
-};
-function criteriaSpecial() {
-  var checkBox = document.getElementById("checkSpecial");
-  if (checkBox.checked == true) {
-      text.style.display = "block";
- } else {
-       text.style.display = "none";
-   }
-};
-//THEN I am presented with a series of prompts for password criteria
-var criteria = function(){
-  console.log ("Select below the criteria you'd like to include in your password :)")
+generateBtn.addEventListener("click", () => {
+  const hasLower = passwordLower.checked;
+  const hasUpper = passwordUpper.checked;
+  const hasNumber = passwordNumber.checked;
+  const hasSpecial = passwordSpecial.checked;
+  const length = +passwordLength.value;
+
+  passwordText.password = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSpecial,
+    length
+  );
+});
+
+// Function generate password to the #password input
+
+function generatePassword(lower, upper, number, special, length) {
+  let generatePassword = '';
+  const criteriaChecked = lower + upper + number + special;
+
+  //console.log("criteriaChecked:", criteriaChecked);
+  // Array for criteria checking
+  const criteriaArray = [{ lower }, { upper }, { number }, { special }].filter
+    (
+      item => Object.values(item)[0]
+    );
+
+  //console.log("criteriaArray:", criteriaArray);
+  // If none of the criteria boxes are checked
+  if (criteriaChecked === 0) {
+    return "";
+  }
+
+  // For-loop used to generate password
+  for (let i = 0; i < length; i += criteriaChecked) {
+    criteriaArray.forEach(type => {
+      const passName = Object.keys(type)[0];
+
+      generatePassword += randomPass[passName]();
+    });
+  }
+  
 }
-criteria();
+const thePassword = generatePassword.slice(0, length);
 
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+return thePassword;
+
+// Functions for criteria--
+// function for lower case
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// function for upper case
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// function for number
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// function for special character
+function getRandomSpecial() {
+  const specials = '!@#$%^&*(){}[]+=<>?/';
+  return specials[Math.floor(Math.random() * specials.length)];
+}
